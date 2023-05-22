5875b29a3251f58ef85f089c34b5247b1e489fe7
Prem Lamsal <premlamsal2@gmail.com>
2023-05-22 20:02:42 +0545

    update

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -8,12 +8,12 @@ use Illuminate\Support\Facades\Auth;
 
 class CheckController extends Controller
 {
-    // public function __construct()
-    // {
+    public function __construct()
+    {
 
-    //     $this->middleware('auth:api');
+        $this->middleware('auth:api');
 
-    // }
+    }
 
     public function stores()
     {
